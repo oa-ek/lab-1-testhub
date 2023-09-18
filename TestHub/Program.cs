caf24eab51a4f915144d83eb838bea95f0319020
okyryliuk <o.kyryliuk.i@gmail.com>
2023-09-18 19:14:50 +0300

    swagger work

--- TestHub/Program.cs
+++ TestHub/Program.cs
@@ -1,10 +1,31 @@
 using TestHub.Infrastructure.Context;
 using TestHub.Infrastructure.Seeders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services
+    .AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+    });
+builder.Services.AddDbContext<TestHubDbContext>(options =>
+{
+    options.UseSqlServer("Server=.;Database=TestHubDb;Trusted_Connection=true;TrustServerCertificate=true;");
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestHub API v1", Version = "v1" });
+});
+
 
 var app = builder.Build();
 
@@ -14,6 +35,14 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestHub API v1");
+    });
 }
 
 app.UseHttpsRedirection();
@@ -29,10 +58,10 @@ using (var context = new TestHubDbContext())
     // Створення сідера і виклик методу Seed()
     var userSeeder = new UserSeeder(context);
     userSeeder.Seed();
-
+    
     var testSeeder = new TestSeeder(context);
     testSeeder.Seed();
-
+    
     var categorySeeder = new CategorySeeder(context);
     categorySeeder.Seed();
     
